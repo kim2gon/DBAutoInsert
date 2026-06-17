69480a89cef73a5194a6956f5d210cd223624315
kim2gon <heegonkim2001@gmail.com>
2026-06-17 15:58:13 +0900

    [Fix] 닷넷4.8로 변경, 데이터 추가 오류 해결

--- obj/Debug/net48/DbAutoInsert.AssemblyInfo.cs
+++ obj/Debug/net48/DbAutoInsert.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbAutoInsert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13aa0ebaee8344548a1e0f2a344c48ed5b2008e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbAutoInsert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbAutoInsert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
