4a85d9796680b1cff58c84c26c1a8ebf23d90759
kim2gon <heegonkim2001@gmail.com>
2026-06-17 10:25:27 +0900

    [Fix]

--- obj/Debug/net8.0-windows/win-x64/DbAutoInsert.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/win-x64/DbAutoInsert.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DbAutoInsert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ede74ce1cd9c4db3faf4bc431cc403b55337a69")]
 [assembly: System.Reflection.AssemblyProductAttribute("DbAutoInsert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DbAutoInsert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
